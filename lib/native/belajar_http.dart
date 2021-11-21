fb8d415f78d894e99a34244bdba0662c5c1fea95
Dian Prawira <dianprawira@sisfo.untan.ac.id>
2021-11-21 14:41:33 +0700

    sudah bisa login dan register juga logout

--- lib/native/belajar_http.dart
+++ lib/native/belajar_http.dart
@@ -1,7 +1,21 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
 void main() async {
-  var url = Uri.parse('https://tg0rgz.deta.dev/');
-  var response = await http.get(url);
-  print(response.statusCode);
+  var fullUrl = 'https://alamatserver/cybercampus/public/api/auth/login';
+  var data = <String, String>{
+    'email': 'bayu@gmail.com',
+    'password': 'password'
+  };
+
+  var res = await http.post(Uri.parse(fullUrl),
+      body: jsonEncode(data),
+      headers: <String, String>{
+        'Content-type': 'application/json; charset=UTF-8',
+        'Accept': 'application/json',
+      });
+  var body = json.decode(res.body);
+
+  print(body['user']['name']);
 }
